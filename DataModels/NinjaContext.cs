45044aeab8bbfec2c4955218409e76b7092a4f49
sandeep gangwar <sandeep.gangwar@silicus.com>
2017-08-19 15:45:57 +0530

    Initial project setup

---
+++ DataModels/NinjaContext.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.Entity;
+using Classes;
+
+
+namespace DataModel
+{
+    public class NinjaContext:DbContext
+    {
+        public DbSet<Ninja> Ninjas { get; set; }
+        public DbSet<Clan> Clans { get; set; }
+        public DbSet<NinjaEquipment> Equipment { get; set; }
+
+    }
+}
---
+++ DomainClasses/NinjaEquipment.cs
@@ -0,0 +1,19 @@
+﻿using Classes.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    public class NinjaEquipment
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public EquipmentType Type { get; set; }
+        [Required]
+        public Ninja Ninja { get; set; }
+    }
+}
