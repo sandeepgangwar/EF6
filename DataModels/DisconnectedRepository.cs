5e5c782290377d1dfcf83b3e3c8ce9a609ff8943
sandeep gangwar <sandeep.gangwar@silicus.com>
2017-08-20 12:09:11 +0530

    Added disconnected repository

---
+++ DataModels/DisconnectedRepository.cs
@@ -0,0 +1,22 @@
+﻿using Classes;
+using DataModel;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataModels
+{
+    public class DisconnectedRepository
+    {
+        public List<Ninja> GetNinjaWithClan()
+        {
+            using (var context = new NinjaContext())
+            {
+                return context.Ninjas.AsNoTracking().Include(n => n.Clan).ToList();
+            }
+        }
+    }
+}
